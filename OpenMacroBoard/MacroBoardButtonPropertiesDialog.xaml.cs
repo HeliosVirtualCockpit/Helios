0b078cd8df987e03a9c79fd75be8614dd08e2d65
wheelchock <6605805+Todd1215@users.noreply.github.com>
2022-01-21 11:37:02 -0500

    Moved some things around to make it easier to understand. Renamed to HeliosMacroBoard.

--- OpenMacroBoard/MacroBoardButtonPropertiesDialog.xaml.cs
+++ OpenMacroBoard/MacroBoardButtonPropertiesDialog.xaml.cs
@@ -8,7 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Forms;
+using Microsoft.Win32;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -57,7 +57,7 @@ namespace GadrocsWorkshop.Helios.Interfaces.OpenMacroBoard
             openDialog.Filter = "Image Files (*.png;*.bmp;*.jpg;*.jpeg;*.gif)|*.png;*.bmp;*.jpg;*.jpeg;*.gif";
             openDialog.InitialDirectory = ConfigManager.ImagePath;
 
-            if (openDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            if (openDialog.ShowDialog() == true)
             {
                 PendingButton.BackgroundImageUri = openDialog.FileName;
             }
