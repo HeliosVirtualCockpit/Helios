c55dd961d2cc227aa1a0d49d12fffc5da17c7365
BlueFinBima <BlueFinBima@users.noreply.github.com>
2022-06-13 13:35:16 +0100

    Namespace correction for AH-64D power lever gauge

--- Helios/Gauges/AH-64D/Power/PowerLever.cs
+++ Helios/Gauges/AH-64D/Power/PowerLever.cs
@@ -14,7 +14,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace GadrocsWorkshop.Helios.Gauges.AH64D.AltimeterPowerLevers
+namespace GadrocsWorkshop.Helios.Gauges.AH64D.PowerLevers
 {
     using GadrocsWorkshop.Helios.ComponentModel;
     using System;
