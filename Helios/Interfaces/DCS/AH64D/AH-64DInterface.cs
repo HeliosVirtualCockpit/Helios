5cc653df6c46092aaf1a3ce04528c62f33a753f8
BlueFinBima <BlueFinBima@users.noreply.github.com>
2022-06-28 14:53:09 +0100

    hif.schema.json update and fix for switch deserializer not setting the flag for incrementalPulse function.

--- Helios/Interfaces/DCS/AH64D/AH-64DInterface.cs
+++ Helios/Interfaces/DCS/AH64D/AH-64DInterface.cs
@@ -922,12 +922,12 @@ namespace GadrocsWorkshop.Helios.Interfaces.DCS.AH64D
             // Vehicles = new string[] { ModuleName, "other aircraft", "another aircraft" };
 
             // see if we can restore from JSON
-            #if (!DEBUG)
+            //#if (!DEBUG)
             if (LoadFunctionsFromJson())
             {
                 return;
             }
-            #endif
+            //#endif
 #region MPDs
             #region Pilot Left
             AddFunction(new PushButton(this, MFD_PLT_LEFT, mpd_commands.T1.ToString("d"), "20", "MFD Left (Pilot)", "Button T1"));
