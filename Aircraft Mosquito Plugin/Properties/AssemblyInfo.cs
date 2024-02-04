a972988bf495f24ec4d502a5c5429646d59f0579
BlueFinBima <BlueFinBima@users.noreply.github.com>
2024-02-04 16:35:55 +0000

    Initial work on the Mosquito interface.

---
+++ Aircraft Mosquito Plugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using GadrocsWorkshop.Helios.ComponentModel;
+
+[assembly: HeliosModule("Helios Mosquito Support for DCS", "Mosquito")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Helios DH98 Mosquito FB MkVI Plugin")]
+[assembly: AssemblyDescription("Resources for Helios relating to Eagle Dynamics Mosquito Module for DCS World")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("github.com/HeliosVirtualCockpit")]
+[assembly: AssemblyProduct("Helios")]
+[assembly: AssemblyCopyright("Copyright (c) 2024 Helios Contributors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("E8A4F183-459B-4CFB-8CBB-7D53DBE77222")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
