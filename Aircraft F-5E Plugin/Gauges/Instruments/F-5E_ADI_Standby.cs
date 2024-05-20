e5295ed3723058814d4e899a391410e5da866932
BlueFinBima <BlueFinBima@users.noreply.github.com>
2024-05-20 18:22:58 +0100

    F-5E completion for testing gauges (apart for the HSI and SAI).

--- Aircraft F-5E Plugin/Gauges/Instruments/F-5E_ADI_Standby.cs
+++ Aircraft F-5E Plugin/Gauges/Instruments/F-5E_ADI_Standby.cs
@@ -21,49 +21,51 @@ namespace GadrocsWorkshop.Helios.Gauges.F5E.Instruments.ADI
     using System.Windows;
     using System.Windows.Media;
 
-    [HeliosControl("Helios.F5E.Instruments.ADI", "ADI", "F-5E Tiger II", typeof(GaugeRenderer),HeliosControlFlags.None)]
-    public class ADIGauge : BaseGauge
+    [HeliosControl("Helios.F5E.Instruments.ADI_Standby", "Standby ADI", "F-5E Tiger II", typeof(GaugeRenderer),HeliosControlFlags.None)]
+    public class ADIStandbyGauge : BaseGauge
     {
         private readonly HeliosValue _pitch;
         private readonly HeliosValue _roll;
-        private double _pitchDouble;
+        private readonly HeliosValue _pitchAdjustment;
         private readonly HeliosValue _offFlag;
 
         private readonly GaugeNeedle _offFlagNeedle;
         private readonly GaugeNeedle _ball;
         private readonly GaugeNeedle _bankNeedle;
+        private readonly GaugeNeedle _wingsNeedle;
 
         private readonly CalibrationPointCollectionDouble _pitchCalibration;
+        private readonly CalibrationPointCollectionDouble _pitchAdjustCalibaration;
 
         private readonly string _gaugeImagePath = "{F-5E}/Gauges/Instruments/Images/";
 
-        public ADIGauge() : this("ADI",new Size(300,300),"Instruments") { }
-        public ADIGauge(string name, Size size, string device)
+        public ADIStandbyGauge() : this("ADI",new Size(300,300),"Instruments") { }
+        public ADIStandbyGauge(string name, Size size, string device)
             : base(name, size)
         {
             Point center = new Point(size.Width / 2d, size.Height / 2d);
 
-            double pitchMax = -235.17935d;
-            _pitchCalibration = new CalibrationPointCollectionDouble(-90d, pitchMax, 90d, pitchMax * -1) {
-                new CalibrationPointDouble(-180d, pitchMax),
-                new CalibrationPointDouble(180d, pitchMax * -1)
-            };
-
-            _ball = new GaugeNeedle($"{_gaugeImagePath}F-5E_ADI_Tape.xaml", center, new Size(235.3092d, 944.2929d), new Point(235.3092d / 2d, 944.2929d / 2d));
-            _ball.Clip = new EllipseGeometry(center, 235.3092d / 2d, 235.3092d / 2d);
+            _pitchCalibration = new CalibrationPointCollectionDouble(-90d, -461d, 90d, 461d);
+            _ball = new GaugeNeedle($"{_gaugeImagePath}F-5E_ADI_Standby_Tape.xaml", center, new Size(235d, 944d), new Point(117d, 944d / 2d));
+            _ball.Clip = new EllipseGeometry(center, 117d, 117d);
             Components.Add(_ball);
 
-            Components.Add(new GaugeImage($"{_gaugeImagePath}F-5E_ADI_Inner_Ring.xaml", new Rect(25d, 25d, 250d, 250d)));
+            _pitchAdjustCalibaration = new CalibrationPointCollectionDouble(-1.0d, -30d, 1.0d, 30d);
 
-            _bankNeedle = new GaugeNeedle($"{_gaugeImagePath}F-5E_ADI_Bank_Pointer.xaml", center, new Size(16d, 105d), new Point(8d, 0d));
+
+            _bankNeedle = new GaugeNeedle($"{_gaugeImagePath}F-5E_ADI_Standby_Bank_Pointer.xaml", center, new Size(16d, 105d), new Point(8d, 0d));
             Components.Add(_bankNeedle);
 
-            Components.Add(new GaugeImage($"{_gaugeImagePath}F-5E_ADI_Wings.xaml", new Rect(27d, 140d, 246d, 34d)));
+            Components.Add(new GaugeImage($"{_gaugeImagePath}F-5E_ADI_Standby_Inner_Ring.xaml", new Rect(25d, 25d, 250d, 250d)));
 
-            Components.Add(new GaugeImage($"{_gaugeImagePath}F-5E_ADI_Outer_Ring.xaml", new Rect(0d, 0d, 300d, 300d)));
+            Components.Add(new GaugeImage($"{_gaugeImagePath}F-5E_ADI_Standby_Wings.xaml", new Rect(27d, 146d, 246d, 34d)));
 
-            _offFlagNeedle = new GaugeNeedle($"{_gaugeImagePath}F-5E_ADI_Off_Flag.xaml", new Point(13.5d, 230d), new Size(78d, 27d), new Point(0d, 13.5d), 90d);
+            Components.Add(new GaugeImage($"{_gaugeImagePath}F-5E_ADI_Standby_Outer_Ring.xaml", new Rect(0d, 0d, 300d, 300d)));
+
+            _offFlagNeedle = new GaugeNeedle($"{_gaugeImagePath}F-5E_ADI_Standby_Off_Flag.xaml", new Point(0d, 150d), new Size(150d, 93d), new Point(0d, 0d), 0d);
             Components.Add(_offFlagNeedle);
+
+
 
             _offFlag = new HeliosValue(this, new BindingValue(false), $"{device}_{name}", "ADI Off Flag", "Indicates the position of the off flag.", "1.0 if displayed.", BindingValueUnits.Numeric);
             _offFlag.Execute += new HeliosActionHandler(OffFlag_Execute);
@@ -72,6 +74,10 @@ namespace GadrocsWorkshop.Helios.Gauges.F5E.Instruments.ADI
             _pitch = new HeliosValue(this, new BindingValue(0d), $"{device}_{name}", "ADI Aircraft Pitch Angle", "Current pitch of the aircraft in degrees.", "(-90 to +90)", BindingValueUnits.Degrees);
             _pitch.Execute += new HeliosActionHandler(Pitch_Execute);
             Actions.Add(_pitch);
+
+            _pitchAdjustment = new HeliosValue(this, new BindingValue(0d), $"{device}_{name}", "ADI Pitch adjustment offset", "Location of pitch reference wings.", "(-1 to 1) 1 full up and -1 is full down.", BindingValueUnits.Numeric);
+            _pitchAdjustment.Execute += new HeliosActionHandler(PitchAdjust_Execute);
+            Actions.Add(_pitchAdjustment);
 
             _roll = new HeliosValue(this, new BindingValue(0d), $"{device}_{name}", "ADI Aircraft Bank Angle", "Current bank of the aircraft in degrees.", "(-180 to +180)", BindingValueUnits.Degrees);
             _roll.Execute += new HeliosActionHandler(Bank_Execute);
@@ -82,16 +88,19 @@ namespace GadrocsWorkshop.Helios.Gauges.F5E.Instruments.ADI
         void OffFlag_Execute(object action, HeliosActionEventArgs e)
         {
             _offFlag.SetValue(e.Value, e.BypassCascadingTriggers);
-            _offFlagNeedle.Rotation = (e.Value.DoubleValue) * -90d;
+            _offFlagNeedle.Rotation = (1-e.Value.DoubleValue) * 15d;
         }
 
         void Pitch_Execute(object action, HeliosActionEventArgs e)
         {
             _pitch.SetValue(e.Value, e.BypassCascadingTriggers);
-            _pitchDouble = _pitchCalibration.Interpolate(e.Value.DoubleValue);
-            _ball.VerticalOffset = _pitchDouble;
+            _ball.VerticalOffset = _pitchCalibration.Interpolate(e.Value.DoubleValue);
         }
-
+        void PitchAdjust_Execute(object action, HeliosActionEventArgs e)
+        {
+            _pitchAdjustment.SetValue(e.Value, e.BypassCascadingTriggers);
+            _wingsNeedle.VerticalOffset = -_pitchAdjustCalibaration.Interpolate(e.Value.DoubleValue);
+        }
         void Bank_Execute(object action, HeliosActionEventArgs e)
         {
             _roll.SetValue(e.Value, e.BypassCascadingTriggers);
