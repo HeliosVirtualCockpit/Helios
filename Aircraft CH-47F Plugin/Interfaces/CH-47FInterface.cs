ab646f5f328907d55845a5fc8d2aa56c7d3365d4
BlueFinBima <BlueFinBima@users.noreply.github.com>
2024-08-31 09:38:08 +0100

    Copyright Date change for the CH-47F interface

--- Aircraft CH-47F Plugin/Interfaces/CH-47FInterface.cs
+++ Aircraft CH-47F Plugin/Interfaces/CH-47FInterface.cs
@@ -1,5 +1,5 @@
 ﻿//  Copyright 2020 Ammo Goettsch
-//  Copyright 2022 Helios Contributors
+//  Copyright 2024 Helios Contributors
 //    
 //  Helios is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
