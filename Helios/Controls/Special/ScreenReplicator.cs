da14cc7b4356720618a0c2cfa058900d9f7066fc
BlueFinBima <BlueFinBima@users.noreply.github.com>
2025-08-01 09:00:23 +0100

    Revert "Raise the Screen Replicator refresh cap to 40Hz (from10 Hz)."
    This reverts commit ed7789e9c9b5ecdefb076dc393dc80954b05d23d.

--- Helios/Controls/Special/ScreenReplicator.cs
+++ Helios/Controls/Special/ScreenReplicator.cs
@@ -154,7 +154,7 @@ namespace GadrocsWorkshop.Helios.Controls.Special
                 {
                     int oldValue = _replicationsPerSecond;
                     _replicationsPerSecond = value;
-                    _millisecondsPerReplication = Math.Max(1000 / _replicationsPerSecond, 25);
+                    _millisecondsPerReplication = Math.Max(1000 / _replicationsPerSecond, 100);
                     OnPropertyChanged("ReplicationsPerSecond", oldValue, value, true);
                 }
             }
